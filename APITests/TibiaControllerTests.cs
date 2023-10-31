d6aebee4495961d915c3be828ae8ca083993e42b
bdzicc <bendziedzicdev@gmail.com>
2023-10-31 13:14:37 -0400

    Added test project and first test.

---
+++ APITests/TibiaControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TibiaAPI.Controllers;
+
+namespace APITests
+{
+    [TestClass]
+    public class TibiaControllerTests
+    {
+        private TibiaController _tibapicontroller;
+        public TibiaControllerTests()
+        {
+            _tibapicontroller = new TibiaController();
+        }
+
+        [TestMethod]
+        public async Task NoCharacterNameShouldReturnError()
+        {
+            //Arrange
+            var charName = string.Empty;
+
+            //Act
+            var resp = await _tibapicontroller.GetCharInfoAsync(charName);
+
+            //Assert
+            Assert.IsNotNull(resp);
+            Assert.AreEqual(400, ((ObjectResult)resp).StatusCode);
+            Assert.AreEqual("Character name must be provided.", ((ObjectResult)resp).Value);
+        }
+    }
+}
